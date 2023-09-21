dbd3da1337817e78f71e1a31ba8a06b62b51ebe6
coderdsraj001 <csdsraj2050@gmail.com>
2023-09-21 14:49:11 +0530

    sub-heading added.

--- js/script.js
+++ js/script.js
@@ -6,11 +6,16 @@ $(document).ready(function () {
         $("main").append(heading);
     }
 
-    $(".formHeading").submit(function (e) {
+    $(".formHeading").on("submit",function (e) {
         var heading = $('input').val()
         $("main").append('<section><h1>' + heading + '</h1><div class="subHeadings"></div></section>')
-        $('.subHeading select').append("<option value='' selected disabled>Please Select Heading</option>")
-        $('.formForm #headings').append("<option value='' selected disabled> Please Select Heading</option>")
+        $('.formSubHeading select').append("<option value='' selected disabled>Please Select Heading</option>")
+        $('section h1').each(function (index) {
+            index = index + 1
+            var subHeading_of_heading = $(this).text()
+            $('.subHeading select').append("<option value=" + index + ">" + subHeading_of_heading + "</option>")
+        })
+
         var selectValue = $('main').html();
         localStorage.setItem("Heading", selectValue);
         e.preventDefault();
@@ -18,15 +23,35 @@ $(document).ready(function () {
     })
 });
 
-$("#headingTextModel").click(function () {
-    $(':input[type="submit"]').prop('disabled', true);
-    $('input[type="text"]').keyup(function () {
-        if ($(this).val() != '') {
-            $(':input[type="submit"]').prop('disabled', false);
-        }
-    });
-})
 
+// sub-heading code :- 
+$("#SubHeadingId").click(function () {
+    $('.formSubHeading option').remove()
+    $('.formSubHeading select').append("<option value='' selected disabled>Please Select Heading</option>")
+    $('section h1').each(function (index) {
+        index = index + 1
+        var subHeading_of_heading = $(this).text()
+        $('.formSubHeading select').append("<option value=" + index + ">" + subHeading_of_heading + "</option>")
+    })
+    var selectValue = $('main').html();
+    localStorage.setItem("Heading", selectValue);
+});
+
+$(document).ready(function () {
+    $(".formSubHeading").on('submit', function (e) {
+        var subHeading_of_heading = $('select option:selected', this).val()
+        var sub_heading = $('input', this).val()
+        $("section:nth-child(" + subHeading_of_heading + ") div.subHeadings").append('<div><h4 class="subheadingtxt">' + sub_heading + '</h4></div>')
+        $('section h4').each(function(index) {
+            index = index + 1
+            $(this).text()
+            var selectValue = $('main').html();
+            localStorage.setItem("Heading", selectValue);
+            e.preventDefault();
+            e.target.reset();
+        })
+    })
+});
 
 
 
