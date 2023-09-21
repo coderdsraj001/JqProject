33f7bc3a6c6b6ec5597ac968ff10d771247a961b
coderdsraj001 <csdsraj2050@gmail.com>
2023-09-21 10:45:27 +0530

    create heading in another way

--- js/script.js
+++ js/script.js
@@ -1,42 +1,34 @@
+// Heading code :-
+
 $(document).ready(function () {
-    $("#myForm").submit(function (event) {     // Event listener for the form submission
-        event.preventDefault(); // Prevent the default form submission
-
-        let heading = $("#heading1").val()
-        let existingData = JSON.parse(localStorage.getItem('formData')) || [];
-
-        existingData.push({
-            heading: heading,
-        });
-
-        localStorage.setItem('formData', JSON.stringify(existingData));// Store the JSON string in LocalStorage
-        displayFormData();
-        $('#exampleModal').modal('hide');
-        $("#heading1").val(''); // Reset the input field value
-    });
-
-    function displayFormData() {
-        let userData = JSON.parse(localStorage.getItem('formData')) || [];
-        let finalData = '';
-        let finalData1 = '';
-
-        userData.forEach(function (formData, index) {
-            finalData += `
-        <div class="row mt-4">
-            <div class="heading mt-4">${formData.heading}</div>
-        </div>
-        `;
-
-        finalData1 += `
-        <select class="custom-select mb-2" id="convoy_list">
-            ${ (index == 0) ? `<option selected>Select Heading</option><option >${formData.heading}</option>` :`<option >${formData.heading}</option>`}
-        </select>
-        `;
-        });
-
-        $('#main').html(finalData);
-        $('#convoy_list').html(finalData1);
+    var heading = localStorage.getItem("Heading");
+    if (heading) {
+        $("main").append(heading);
     }
 
-    displayFormData();
+    $(".formHeading").submit(function (e) {
+        var heading = $('input').val()
+        $("main").append('<section><h1>' + heading + '</h1><div class="subHeadings"></div></section>')
+        $('.subHeading select').append("<option value='' selected disabled>Please Select Heading</option>")
+        $('.formForm #headings').append("<option value='' selected disabled> Please Select Heading</option>")
+        var selectValue = $('main').html();
+        localStorage.setItem("Heading", selectValue);
+        e.preventDefault();
+        e.target.reset();
+    })
 });
+
+$("#headingTextModel").click(function () {
+    $(':input[type="submit"]').prop('disabled', true);
+    $('input[type="text"]').keyup(function () {
+        if ($(this).val() != '') {
+            $(':input[type="submit"]').prop('disabled', false);
+        }
+    });
+})
+
+
+
+
+
+ 
